807ddcd81832780304ff269f49086271b1d01a6a
Dimitris N <dnikolos@gmail.com>
2014-09-07 14:58:24 +0300

    One and only
    A really small project that solves one problem.

---
+++ quizsolver.py
@@ -0,0 +1,22 @@
+
+def quizsolver():
+    posf = [1,2,3,4,5,6,7,8,9]
+    pose = [0,0,0,1,2,3,4,6,8]
+    posc = [1,4,9,6,5,6,9,4,1]
+    for (i,f) in enumerate(posf):
+        e = pose[i]
+        c = posc[i]
+        for a in range(0,10):
+            for b in range(0,10):
+                for c in range(0,10):
+                    for d in range(0,10):
+                        for g in range(0,10):
+                            if ((10*a+b)*(10*b+c)==(d*100+a*10+d)):
+                                if ((10*e+c)/f == f):
+                                    if ((100*b+10*c+d)*g==1000*b+100*b+10*e+a):
+                                        if ((10*a+b)*(10*e+c)==(100*b+10*c+d)):
+                                            print(''.join([str(a),str(b),str(c),str(d),str(e),
+                                                str(f),str(g)]))
+                    
+
+quizsolver()
