132b7da0de765f86845df855940387a7afd47f20
Dimitris N <dnikolos@gmail.com>
2014-09-09 16:19:20 +0300

    c is computed
    c is computed

--- quizsolver.py
+++ quizsolver.py
@@ -8,15 +8,14 @@ def quizsolver():
         c = posc[i]
         for a in range(0,10):
             for b in range(0,10):
-                for c in range(0,10):
-                    for d in range(0,10):
-                        for g in range(0,10):
-                            if ((10*a+b)*(10*b+c)==(d*100+a*10+d)):
-                                if ((10*e+c)/f == f):
-                                    if ((100*b+10*c+d)*g==1000*b+100*b+10*e+a):
-                                        if ((10*a+b)*(10*e+c)==(100*b+10*c+d)):
-                                            print(''.join([str(a),str(b),str(c),str(d),str(e),
-                                                str(f),str(g)]))
+                for d in range(0,10):
+                    for g in range(0,10):
+                        if ((10*a+b)*(10*b+c)==(d*100+a*10+d)):
+                            if ((10*e+c)/f == f):
+                                if ((100*b+10*c+d)*g==1000*b+100*b+10*e+a):
+                                    if ((10*a+b)*(10*e+c)==(100*b+10*c+d)):
+                                        print(''.join([str(a),str(b),str(c),str(d),str(e),
+                                            str(f),str(g)]))
                     
 
 quizsolver()
