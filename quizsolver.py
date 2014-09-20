af165539771b9213defc19584ef67dc66ba4931b
Dimitris N <dnikolos@gmail.com>
2014-09-20 06:40:50 +0300

    c,e without arrays

--- quizsolver.py
+++ quizsolver.py
@@ -6,34 +6,31 @@ def quizsolver1():
           for e in range(0,10):
             for f in range(1,10):
               for g in range(0,10):
-                if ((10*a+b)*(10*b+c)==(d*100+a*10+d)):
-                  if ((10*e+c)/f == f):
-                    if ((100*b+10*c+d)*g==1000*b+100*b+10*e+a):
-                      if ((10*a+b)*(10*e+c)==(100*b+10*c+d)):
-                        if ((10*b+c)/f == g):
-                          if ((100*d+10*a+d) * f == (1000*b+100*b+10*e+a)):
-                            print(''.join([str(a),str(b),str(c),
-                              str(d),str(e),str(f),str(g)]))
+                if (((10*a+b)*(10*b+c)==d*100+a*10+d)
+                  and ((10*e+c)/f == f) 
+                  and ((100*b+10*c+d)*g==1000*b+100*b+10*e+a)
+                  and ((10*a+b)*(10*e+c)==100*b+10*c+d)
+                  and ((10*b+c)/f == g) 
+                  and ((100*d+10*a+d) * f == 1000*b+100*b+10*e+a)):
+                    print(''.join([str(a),str(b),str(c),str(d),str(e),
+                    str(f),str(g)]))
 
 def quizsolver2():
-  posf = [1,2,3,4,5,6,7,8,9]
-  pose = [0,0,0,1,2,3,4,6,8]
-  posc = [1,4,9,6,5,6,9,4,1]
-  for (i,f) in enumerate(posf):
-    e = pose[i]
-    c = posc[i]
-    for a in range(0,10):
-      for b in range(0,10):
-        for d in range(0,10):
-          for g in range(0,10):
-            if ((10*a+b)*(10*b+c)==(d*100+a*10+d)):
-              if ((10*e+c)/f == f):
-                if ((100*b+10*c+d)*g==1000*b+100*b+10*e+a):
-                  if ((10*a+b)*(10*e+c)==(100*b+10*c+d)):
-                    if ((10*b+c)/f == g):
-                      if ((100*d+10*a+d) * f == (1000*b+100*b+10*e+a)):
-                        print(''.join([str(a),str(b),str(c),str(d),str(e),
-                          str(f),str(g)]))
+  for a in range(0,10):
+    for b in range(0,10):
+      for d in range(0,10):
+		for f in range(1,10):
+		  for g in range(0,10):
+			c = (f*f) % 10
+			e = (f*f) / 10
+			if (((10*a+b)*(10*b+c)==d*100+a*10+d)
+			  and ((10*e+c)/f == f) 
+			  and ((100*b+10*c+d)*g==1000*b+100*b+10*e+a)
+			  and ((10*a+b)*(10*e+c)==100*b+10*c+d)
+			  and ((10*b+c)/f == g) 
+			  and ((100*d+10*a+d) * f == 1000*b+100*b+10*e+a)):
+			  print(''.join([str(a),str(b),str(c),str(d),
+			    str(e),str(f),str(g)]))
                     
 quizsolver1()
 quizsolver2()
