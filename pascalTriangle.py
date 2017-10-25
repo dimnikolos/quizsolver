e2a0cc14fd1dc116e756b8bf063cbd69855f994f
dimnikolos <dnikolos@gmail.com>
2017-10-25 09:33:22 +0300

    pascalTriangle

---
+++ pascalTriangle.py
@@ -0,0 +1,19 @@
+def pascalTriangle(n):
+	if n == 0:
+		return([1])
+	else:
+		prevRow = pascalTriangle(n-1)
+		nRow = []
+		for (i,num) in enumerate(prevRow):
+			if i == 0:
+				nRow.append(prevRow[i])
+			else:
+				nRow.append(prevRow[i]+prevRow[i-1])
+
+				
+		nRow.append(1)
+		return(nRow)
+
+while True:
+	n = int(input('Give n:'))
+	print(",".join(pascalTriangle(n)))
